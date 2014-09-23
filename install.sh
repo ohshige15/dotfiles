fa601a92aa09ae1f504a387006b3a92394ee05b1
o-tomox <satoshi.o.xxx@gmail.com>
2014-09-23 13:32:24 +0900

    インストールファイルにgitignoreを追加

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ echo 'start making symbolic links'
 
 echo '.gitconfig'
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+echo '.gitignore'
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
 echo '.zshrc'
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
