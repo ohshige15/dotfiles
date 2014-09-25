f868069ca13a313a50d328eaf384142c949d1b25
o-tomox <satoshi.o.xxx@gmail.com>
2014-09-25 22:31:52 +0900

    インストールファイルにzshrc.localとvimrcを追加

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ echo '.gitignore'
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 echo '.zshrc'
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+echo '.zshrc.local'
+ln -sf ~/dotfiles/.zshrc.local ~/.zshrc.local
+echo '.vimrc'
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 echo 'end making'
